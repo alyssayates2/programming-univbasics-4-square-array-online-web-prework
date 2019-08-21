597041b54221510aaa85a703316d8794e78bf645
Alyssa Yates <bosslys@icloud.com>
2019-08-21 22:41:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
+  array.map do |num|
+    if (num.square?)
+      num
+    else
+      num ** 2
+    end
+  end
+  counter = 0
 
+  while counter < array.length do
+    puts array[counter]
+    counter +=1
+  end
+end
 end
